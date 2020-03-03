794264f72be38fc6c69b158487eacd3a8e639312
Andreas Eriksson <andreas@codered.se>
2020-03-03 12:23:34 +0100

    Draggable setup

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,6 +11,15 @@ import css from "../css/app.css"
 //
 import "phoenix_html"
 
+import interact from "interactjs"
+
+interact('.item').draggable({
+  onmove(event) {
+    dragMoveListener(event)
+    // console.log(event.pageX, event.pageY)
+  }
+})
+
 // Import local files
 //
 // Local files can be imported directly using relative paths, for example:
@@ -24,3 +33,20 @@ let Hooks = {}
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content");
 let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}, hooks: Hooks});
 liveSocket.connect()
+
+
+function dragMoveListener (event) {
+  var target = event.target
+  // keep the dragged position in the data-x/data-y attributes
+  var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
+  var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
+
+  // translate the element
+  target.style.webkitTransform =
+    target.style.transform =
+      'translate(' + x + 'px, ' + y + 'px)'
+
+  // update the posiion attributes
+  target.setAttribute('data-x', x)
+  target.setAttribute('data-y', y)
+}
--- lib/tutorial_web/router.ex
+++ lib/tutorial_web/router.ex
@@ -19,6 +19,7 @@ defmodule TutorialWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    get "/draggable", PageController, :draggable
 
     live "/products", ProductListLive # NEEDS TO BE ABOVE
     resources "/products", ProductController
